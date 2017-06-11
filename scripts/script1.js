c015d3357657815866dd12bf971a3a86bc39aa4e
John <john@johnwgennell.com>
2017-06-10 21:37:30 -0400

    added all scripts to scripts1

--- scripts/script1.js
+++ scripts/script1.js
@@ -3,3 +3,18 @@ module.exports = function(robot) {
       return res.send("Hi there!");
     });
 }
+
+bot.respond(/What's your favorite food?/, function(res) {
+  return res.send("Dynamite, boi.");
+});
+
+bot.respond(/Hi johnny5! My name is (.*)/i, function(msg) {
+  var name;
+  name = msg.match[1];
+  if (name == "johnny5"){
+    return msg.send("You're not johnny5--I'm johnny5!");
+  } else {
+    return msg.reply("Nice to meet you, " + name + "!");
+  }
+
+});
