2e86d99e9de39445de5a9171d4e8b6264d72a7f7
John <john@johnwgennell.com>
2017-06-12 13:58:58 -0400

    respond based on name entry

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,31 +1,31 @@
-/* 1) johnny5 replies to Hello! */
+// 1) johnny5 replies to Hello!
 module.exports = function(robot) {
     robot.hear(/Hello!/, function(res) {
       return res.send("Hi there!");
     });
 }
 
-/* 2) johnny5 responds to Hello, no exclamation point */
+// 2) johnny5 responds to Hello, no exclamation point
 module.exports = function(robot) {
     robot.hear(/Hello/, function(res) {
       return res.send("Hi there!");
     });
 }
 
-/* 3) Hears "how is the weather" and replies */
+// 3) Hears "how is the weather" and replies
 module.exports = function(robot) {
     robot.hear(/How is the weather?/, function(res) {
       return res.send("It's always sunny in Philadelphia.");
     });
 }
 
-/* 4) Adds two numbers together */
-bot.respond(/add (.*) and (.*)/i, function(msg) {
-  var a;
-  var b;
-  a = parseInt(msg.match[1]);
-  b = parseInt(msg.match[2]);
-  c = a + b
-
-  return msg.reply(a + " plus " + b + " = " + c);
+// johnny5 responds to name
+robot.respond(/Hi johnny5! My name is (.*)/i, function(msg) {
+     var name;
+     name = msg.match[1];
+     if (name == "johnny5"){
+       return msg.send("You're not johnny5--I'm johnny5!");
+     } else {
+       return msg.reply("Nice to meet you, " + name + "!");
+     }
 });
