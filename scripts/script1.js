f538b786c6196d454f5ab4efdf65b78174376c6b
John <john@johnwgennell.com>
2017-06-12 17:29:10 -0400

    coffeescript is added

--- scripts/script1.js
+++ scripts/script1.js
@@ -5,13 +5,6 @@ module.exports = function(robot) {
     });
 }
 
-// 2) johnny5 responds to Hello, no exclamation point
-module.exports = function(robot) {
-    robot.hear(/Hello/, function(res) {
-      return res.send("Hi there!");
-    });
-}
-
 // 3) Hears "how is the weather" and replies
 module.exports = function(robot) {
     robot.hear(/How is the weather?/, function(res) {
