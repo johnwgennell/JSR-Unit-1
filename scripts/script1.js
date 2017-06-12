e7ae100f645a0597abdba91dd0cc50e6a5b05e3d
John <john@johnwgennell.com>
2017-06-12 16:21:18 -0400

    removed a coffescript line from js

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,23 +1,18 @@
 // 1) johnny5 replies to Hello!
 module.exports = function(robot) {
-    robot.hear(/Hello!/, function(res) {
+    robot.hear(/Hello!hello|hello!|Hi!|hi!|Hi|hi|Hey!|hey!|Hey|hey/, function(res) {
       return res.send("Hi there!");
     });
 }
 
-// 2) johnny5 resonds to various greetings
-robot.respond(/hello|hello!|Hi!|hi!|Hi|hi|Hey!|hey!|Hey|hey/i, function(res) {
-      return res.reply(res.random(greetings));
-    });
-
-// 3) johnny5 responds to Hello, no exclamation point
+// 2) johnny5 responds to Hello, no exclamation point
 module.exports = function(robot) {
     robot.hear(/Hello/, function(res) {
       return res.send("Hi there!");
     });
 }
 
-// 4) Hears "how is the weather" and replies
+// 3) Hears "how is the weather" and replies
 module.exports = function(robot) {
     robot.hear(/How is the weather?/, function(res) {
       return res.send("It's always sunny in Philadelphia.");
