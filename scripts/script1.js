b286f0cb8aa2c86712a0fc60d9fde132c0f1ef9d
John <john@johnwgennell.com>
2017-06-12 13:03:35 -0400

    third action test

--- scripts/script1.js
+++ scripts/script1.js
@@ -11,3 +11,10 @@ module.exports = function(robot) {
       return res.send("Hi there!");
     });
 }
+
+/* 3) Hears "how is the weather" and replies */
+module.exports = function(robot) {
+    robot.hear(/How is the weather?/, function(res) {
+      return res.send("It's always sunny in Philadelphia.");
+    });
+}
