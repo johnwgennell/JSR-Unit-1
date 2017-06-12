d2b231e434e47c16d8e56c49ca90da606751c036
John <john@johnwgennell.com>
2017-06-12 16:31:34 -0400

    removing |or| conditionals

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,6 +1,6 @@
 // 1) johnny5 replies to Hello!
 module.exports = function(robot) {
-    robot.hear(/Hello!hello|hello!|Hi!|hi!|Hi|hi|Hey!|hey!|Hey|hey/, function(res) {
+    robot.hear(/Hello!/, function(res) {
       return res.send("Hi there!");
     });
 }
