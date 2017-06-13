82e02cdaa95dfb846ff105b8af4e2929e3b26ffe
John <john@johnwgennell.com>
2017-06-13 09:50:45 -0400

    adding two more .respond methods

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,25 +1,47 @@
 module.exports = function(robot) {
 
 // 1) johnny5 replies to Hello!
-    robot.hear(/Hello!/, function(res) {
-      return res.send("Hi there!");
-    });
+robot.hear(/Hello!/, function(res) {
+  return res.send("Hi there!");
+});
 
 // 2) johnny5 responds to various greetings (° ͜ʖ ͡°)
-    robot.respond(/Hello|hello!|hello|Hi|hi!|hi|Hey!|Hey|hey!|hey/i, function(greeting) {
-  		return greeting.send("Hi there!");
-    });
+robot.respond(/Hello|hello!|hello|Hi|hi!|hi|Hey!|Hey|hey!|hey/i, function(greeting) {
+  return greeting.send("Hi there!");
+});
 
 // 3) Hears "how is the weather" and replies
-    robot.hear(/How is the weather?/, function(weather) {
-      return weather.send("It's always sunny in Philadelphia.");
-    });
+robot.hear(/How is the weather?/, function(weather) {
+  return weather.send("It's always sunny in Philadelphia.");
+});
 
 // 4) Sending an image based off a request ¯\_(ツ)_/¯
-	robot.hear(/Show me a picture of yourself./, function(res) {
-		var johnny5Img = "http://vignette3.wikia.nocookie.net/robotsupremacy/images/7/7f/Johnny5-2.jpg/revision/latest?cb=20120321192157";
-		return res.send("This is me, in the flesh! Well, I don't actually have any flesh, but you know what I mean ;) \n " + johnny5Img);
-	});
+robot.hear(/Show me a picture of yourself./, function(res) {
+  var johnny5Img = "http://vignette3.wikia.nocookie.net/robotsupremacy/images/7/7f/Johnny5-2.jpg/revision/latest?cb=20120321192157";
+  return res.send("This is me, in the flesh! Well, I don't actually have any flesh, but you know what I mean ;) \n " + johnny5Img);
+});
+
+// 5) Responding directly to name
+bot.respond(/Hi johnny5! My name is (.*)/i, function(msg) {
+   var name;
+   name = msg.match[1];
+   if (name == "johnny5"){
+     return msg.send("You're not johnny5, I am!");
+   } else {
+     return msg.reply("Nice to meet you, " + name + "!");
+   }
+ });
+
+// 6) Adding two numbers together
+bot.respond(/add (.*) and (.*)/i, function(msg) {
+  var a;
+  var b;
+  a = parseInt(msg.match[1]);
+  b = parseInt(msg.match[2]);
+  c = a + b
+
+  return msg.reply(a + " plus " + b + " = " + c);
+});
 
 
 
