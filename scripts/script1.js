39c0453a9cc59bc7ccba50634a60175443fa5a6e
John <john@johnwgennell.com>
2017-06-13 10:33:45 -0400

    another roll a die edit

--- scripts/script1.js
+++ scripts/script1.js
@@ -19,7 +19,7 @@ robot.hear(/Show me a picture of yourself./, function(res) {
 // 4) Rolling a die on request
 robot.respond(/Roll a die.|Roll a die|roll a die/i, function(res) {
   var dice = {
-    function (roll) {
+    roll: function () {
       var randomNumber = Math.floor(Math.random() * 6) + 1;
       return res.send("You rolled a ;) \n " + randomNumber + "!");
     }
