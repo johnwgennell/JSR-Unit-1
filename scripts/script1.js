9e6b6ef6d566306ceeeeb5d12243392cdc384866
John <john@johnwgennell.com>
2017-06-13 10:08:21 -0400

    roll a die function

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,27 +1,31 @@
 module.exports = function(robot) {
 
-// 1) johnny5 replies to Hello!
-robot.hear(/Hello!/, function(res) {
-  return res.send("Hi there!");
-});
-
-// 2) johnny5 responds to various greetings (° ͜ʖ ͡°)
+// 1) johnny5 responds to various greetings (° ͜ʖ ͡°)
 robot.respond(/Hello|hello!|hello|Hi|hi!|hi|Hey!|Hey|hey!|hey/i, function(greeting) {
   return greeting.send("Hi there!");
 });
 
-// 3) Hears "how is the weather" and replies
+// 2) Hears "how is the weather" and replies
 robot.hear(/How is the weather?/, function(weather) {
   return weather.send("It's always sunny in Philadelphia.");
 });
 
-// 4) Sending an image based off a request ¯\_(ツ)_/¯
+// 3) Sending an image based off a request ¯\_(ツ)_/¯
 robot.hear(/Show me a picture of yourself./, function(res) {
   var johnny5Img = "http://vignette3.wikia.nocookie.net/robotsupremacy/images/7/7f/Johnny5-2.jpg/revision/latest?cb=20120321192157";
   return res.send("This is me, in the flesh! Well, I don't actually have any flesh, but you know what I mean ;) \n " + johnny5Img);
 });
 
-
+// 4) Rolling a die on request
+robot.respond(/Roll a die.|Roll a die|roll a die/i, function(res) {
+  var dice = {
+    sides: 6,
+    roll: function () {
+      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+      return randomNumber;
+    }
+  }
+});
 
 
 
