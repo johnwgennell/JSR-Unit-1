837873edbeb59e69ca47476deaf935507acbcf70
John <john@johnwgennell.com>
2017-06-13 11:14:56 -0400

    adjust numbering in the comments

--- scripts/script1.js
+++ scripts/script1.js
@@ -16,18 +16,18 @@ robot.respond(/Hi johnny5! My name is (.*)/i, function(msg) {
   }
 });
 
-// 2) Hears "how is the weather" and replies
+// 3) Hears "how is the weather" and replies
 robot.hear(/How is the weather?/, function(weather) {
   return weather.send("It's always sunny in Philadelphia.");
 });
 
-// 3) Sending an image based off a request ¯\_(ツ)_/¯
+// 4) Sending an image based off a request ¯\_(ツ)_/¯
 robot.hear(/Show me a picture of yourself./, function(res) {
   var johnny5Img = "http://vignette3.wikia.nocookie.net/robotsupremacy/images/7/7f/Johnny5-2.jpg/revision/latest?cb=20120321192157";
   return res.send("This is me, in the flesh! Well, I don't actually have any flesh, but you know what I mean ;) \n " + johnny5Img);
 });
 
-// 4) Rolling a die on request
+// 5) Rolling a die on request
 robot.respond(/Roll a die.|Roll a die|roll a die|Roll again.|Roll again|roll again/i, function(res) {
     var total = 0;
     total = Math.floor((Math.random() * 6) + 1);
